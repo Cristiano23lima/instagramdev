1e4e0eb6227272198746451589b2e77892b90280
Cristiano Rodrigues <dj7cristiano@gmail.com>
2019-12-19 18:46:17 -0300

    Adicionando o link do backend em produção

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -58,7 +58,7 @@ class Feed extends Component{
                             <img src={more} alt="Mais"/>
                         </header>
 
-                            <img src={`http://localhost:8081/files/${post.image}`} alt="" />
+                            <img src={`https://backendinstagramdev.herokuapp.com/files/${post.image}`} alt="" />
 
                         <footer>
                             <div className='actions'>
